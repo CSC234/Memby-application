0040f7f78b00f34d9406c0cc22a833f280951f7f
Wirawat Jaiarree <katak6565@gmail.com>
2021-03-21 17:36:52 +0700

    [UPDATE] landing Screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFFFFFFFF);
 const kPrimaryLightColor = Color(0xFF6E7CE4);
 const kPrimaryFont = Color(0xFF5A5A5A);
+const kPrimaryMain = Color(0xFF2336C0);
 const kPrimaryFontLight = Color(0xFFB7B7B7);
--- lib/screens/landingScreen.dart
+++ lib/screens/landingScreen.dart
@@ -12,6 +12,87 @@ class Landing extends StatelessWidget {
       backgroundColor: kPrimaryColor,
       body: Column(
         children: [
+          Container(
+            height: 400,
+            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+              color: kPrimaryMain,
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  height: 200,
+                  width: 300,
+                  child: Container(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 50,
+                        ),
+                        Text(
+                          'Welcome to Your Business',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontFamily: 'Alef-Regular'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 15,
+                  bottom: 5,
+                  height: 300,
+                  width: 350,
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Godchapong',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 60,
+                              fontFamily: 'Alef-Regular'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: 0,
+                  top: 180,
+                  width: 300,
+                  child: Container(
+                    child: Column(
+                      children: [
+                        Text(
+                          'Company',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 60,
+                              fontFamily: 'Alef-Regular'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // Positioned(
+                //     // left: 10,
+                //     // width: 10,
+                //     child: Container(
+                //   decoration: BoxDecoration(
+                //       image: DecorationImage(
+                //     image: AssetImage('assets/images/Invest.png'),
+                //   )),
+                // )),
+              ],
+            ),
+          ),
           RoundedButton(
             color: kPrimaryLightColor,
             buttonHight: 60,
