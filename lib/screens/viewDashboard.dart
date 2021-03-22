c953489879f4f43e7a2d65a3ba80398627b21a23
Gorgun <yosathorn.pengma@mail.kmutt.ac.th>
2021-03-22 20:02:04 +0700

    [UPDATE] Remove back buttom(view dashboard)

--- lib/screens/viewDashboard.dart
+++ lib/screens/viewDashboard.dart
@@ -10,26 +10,9 @@ class Dashboard extends StatelessWidget {
     return Scaffold(
       backgroundColor: kPrimaryMain,
       appBar: AppBar(
-        title: Text(
-          'Back',
-          style: TextStyle(
-              color: Colors.white, fontSize: 20, fontFamily: 'Alef-Regular'),
-        ),
         backgroundColor: kPrimaryMain,
         elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) {
-                  return Landing();
-                },
-              ),
-            );
-          },
-        ),
+        automaticallyImplyLeading: false,
       ),
       body: Body(),
     );
