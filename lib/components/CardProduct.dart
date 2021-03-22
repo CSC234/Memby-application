e510288887fc6192d4b3fbf0e44b5850ceab7170
Wirawat Jaiarree <katak6565@gmail.com>
2021-03-22 21:09:03 +0700

    [UPDATE] card of product

--- lib/components/CardProduct.dart
+++ lib/components/CardProduct.dart
@@ -24,184 +24,195 @@ class CardProductButton extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    return Container(
-        child: FlatButton(
-      onPressed: press,
-      child: SizedBox(
-        width: width - 25,
-        height: height * (46 / 100),
-        child: DecoratedBox(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    spreadRadius: 1,
-                    blurRadius: 5,
-                    offset: Offset(0, 5),
-                  ),
-                ]),
-            child: Row(
-              children: [
-                Container(
-                    margin: EdgeInsets.only(left: 20),
-                    child: Container(
-                        child: Column(
-                      children: [
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(children: [
-                          Text(
-                            'Product 1',
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: kPrimaryMain,
-                                fontWeight: FontWeight.bold),
+    return Column(children: [
+      Container(
+          child: FlatButton(
+        onPressed: press,
+        child: SizedBox(
+          width: width - 25,
+          height: height * (46 / 100),
+          child: DecoratedBox(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 5),
+                    ),
+                  ]),
+              child: Row(
+                children: [
+                  Container(
+                      margin: EdgeInsets.only(left: 20),
+                      child: Container(
+                          child: Column(
+                        children: [
+                          SizedBox(
+                            height: 20,
                           ),
-                          Container(
-                              margin: EdgeInsets.only(left: width * (25 / 100)),
-                              child: Row(children: [
-                                TextButton(
-                                  child: Container(
-                                    margin: EdgeInsets.symmetric(horizontal: 1),
-                                    width: 50,
-                                    height: 50,
-                                    child: Center(
-                                        child: Text(
-                                      '+',
-                                      style: TextStyle(
-                                          fontSize: 15, color: Colors.white),
-                                    )),
-                                    decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: kPrimaryMain),
-                                  ),
-                                ),
-                                TextButton(
-                                  child: Container(
-                                    margin: EdgeInsets.symmetric(horizontal: 1),
-                                    width: 50,
-                                    height: 50,
-                                    child: Center(
-                                        child: Text(
-                                      '-',
-                                      style: TextStyle(
-                                          fontSize: 15, color: Colors.white),
-                                    )),
-                                    decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: Color(0xFF6961D6)),
-                                  ),
-                                ),
-                              ])),
-                        ]),
-                        Row(
-                          children: [
-                            Column(
-                              children: [
-                                Container(
-                                    margin: EdgeInsets.symmetric(vertical: 10),
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 15, vertical: 5),
-                                    width: width * (50 / 100),
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                      color: Colors.black12,
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                    child: TextField(
-                                      // controller: passwordController,
-                                      decoration: InputDecoration(
-                                          hintText: 'product name...',
-                                          border: InputBorder.none),
-                                    )),
-                                Container(
-                                    margin: EdgeInsets.symmetric(vertical: 10),
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 15, vertical: 5),
-                                    width: width * (50 / 100),
-                                    height: height * (20 / 100),
-                                    decoration: BoxDecoration(
-                                      color: Colors.black12,
-                                      borderRadius: BorderRadius.circular(5),
-                                    ),
-                                    child: TextField(
-                                      // controller: passwordController,
-                                      obscureText: true,
-                                      onChanged: (String value) {
-                                        // setState(() {
-                                        // _value = value;
-                                        // });
-                                        // widget.onChanged(value);
-                                      },
-                                      decoration: InputDecoration(
-                                          hintText: 'detail of product...',
-                                          border: InputBorder.none),
-                                    )),
-                              ],
+                          Row(children: [
+                            Text(
+                              'Product 1',
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: kPrimaryMain,
+                                  fontWeight: FontWeight.bold),
                             ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            Column(
-                              children: [
-                                Container(
-                                    margin: EdgeInsets.symmetric(vertical: 10),
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 15, vertical: 5),
-                                    width: width * (30 / 100),
-                                    height: height * (20 / 100),
-                                    decoration: BoxDecoration(
-                                      color: Colors.black12,
-                                      borderRadius: BorderRadius.circular(5),
+                            Container(
+                                margin:
+                                    EdgeInsets.only(left: width * (25 / 100)),
+                                child: Row(children: [
+                                  TextButton(
+                                    child: Container(
+                                      margin:
+                                          EdgeInsets.symmetric(horizontal: 1),
+                                      width: 50,
+                                      height: 50,
+                                      child: Center(
+                                          child: Text(
+                                        '+',
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      )),
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: kPrimaryMain),
                                     ),
-                                    child: TextField(
-                                      // controller: passwordController,
-                                      obscureText: true,
-                                      onChanged: (String value) {
-                                        // setState(() {
-                                        // _value = value;
-                                        // });
-                                        // widget.onChanged(value);
-                                      },
-                                      decoration: InputDecoration(
-                                          hintText: 'Add picture',
-                                          border: InputBorder.none),
-                                    )),
-                                Container(
-                                    margin: EdgeInsets.symmetric(vertical: 10),
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 15, vertical: 5),
-                                    width: width * (30 / 100),
-                                    height: 60,
-                                    decoration: BoxDecoration(
-                                      color: Colors.black12,
-                                      borderRadius: BorderRadius.circular(5),
+                                    onPressed: press,
+                                  ),
+                                  TextButton(
+                                    child: Container(
+                                      margin:
+                                          EdgeInsets.symmetric(horizontal: 1),
+                                      width: 50,
+                                      height: 50,
+                                      child: Center(
+                                          child: Text(
+                                        '-',
+                                        style: TextStyle(
+                                            fontSize: 15, color: Colors.white),
+                                      )),
+                                      decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          color: Color(0xFF6961D6)),
                                     ),
-                                    child: TextField(
-                                      // controller: passwordController,
-                                      obscureText: true,
-                                      onChanged: (String value) {
-                                        // setState(() {
-                                        // _value = value;
-                                        // });
-                                        // widget.onChanged(value);
-                                      },
-                                      decoration: InputDecoration(
-                                          hintText: 'price...',
-                                          border: InputBorder.none),
-                                    )),
-                              ],
-                            )
-                          ],
-                        )
-                      ],
-                    )))
-              ],
-            )),
-      ),
-    ));
+                                  ),
+                                ])),
+                          ]),
+                          Row(
+                            children: [
+                              Column(
+                                children: [
+                                  Container(
+                                      margin:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 15, vertical: 5),
+                                      width: width * (50 / 100),
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black12,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: TextField(
+                                        // controller: passwordController,
+                                        decoration: InputDecoration(
+                                            hintText: 'product name...',
+                                            border: InputBorder.none),
+                                      )),
+                                  Container(
+                                      margin:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 15, vertical: 5),
+                                      width: width * (50 / 100),
+                                      height: height * (20 / 100),
+                                      decoration: BoxDecoration(
+                                        color: Colors.black12,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: TextField(
+                                        // controller: passwordController,
+                                        obscureText: true,
+                                        onChanged: (String value) {
+                                          // setState(() {
+                                          // _value = value;
+                                          // });
+                                          // widget.onChanged(value);
+                                        },
+                                        decoration: InputDecoration(
+                                            hintText: 'detail of product...',
+                                            border: InputBorder.none),
+                                      )),
+                                ],
+                              ),
+                              SizedBox(
+                                width: 15,
+                              ),
+                              Column(
+                                children: [
+                                  Container(
+                                      margin:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 15, vertical: 5),
+                                      width: width * (30 / 100),
+                                      height: height * (20 / 100),
+                                      decoration: BoxDecoration(
+                                        color: Colors.black12,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: TextField(
+                                        // controller: passwordController,
+                                        obscureText: true,
+                                        onChanged: (String value) {
+                                          // setState(() {
+                                          // _value = value;
+                                          // });
+                                          // widget.onChanged(value);
+                                        },
+                                        decoration: InputDecoration(
+                                            hintText: 'Add picture',
+                                            border: InputBorder.none),
+                                      )),
+                                  Container(
+                                      margin:
+                                          EdgeInsets.symmetric(vertical: 10),
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 15, vertical: 5),
+                                      width: width * (30 / 100),
+                                      height: 60,
+                                      decoration: BoxDecoration(
+                                        color: Colors.black12,
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      child: TextField(
+                                        // controller: passwordController,
+                                        obscureText: true,
+                                        onChanged: (String value) {
+                                          // setState(() {
+                                          // _value = value;
+                                          // });
+                                          // widget.onChanged(value);
+                                        },
+                                        decoration: InputDecoration(
+                                            hintText: 'price...',
+                                            border: InputBorder.none),
+                                      )),
+                                ],
+                              )
+                            ],
+                          ),
+                        ],
+                      )))
+                ],
+              )),
+        ),
+      )),
+      SizedBox(height: 30),
+    ]);
   }
 }
--- lib/screens/addProductScreen.dart
+++ lib/screens/addProductScreen.dart
@@ -15,43 +15,71 @@ class AddProductList extends StatefulWidget {
 
 class Product {
   String question;
-  List<String> answer;
+  List<double> answer;
   Product({this.question, this.answer});
 }
 
 class _AddProductList extends State<AddProductList> {
+  double val = 0;
+  List<Product> product = [
+    Product(question: "test", answer: [0])
+  ];
+  void change() {
+    setState(() {
+      product.add(Product(question: 'aasdg', answer: [val]));
+      val += 1;
+      // print(val);
+      for (var i = 0; i < product.length; i++) {
+        print(product[i].answer.toString());
+      }
+      print('---------------------');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: kPrimaryColor,
-        body: Container(
-          child: Align(
-            alignment: Alignment.center,
-            child: Container(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Text(
-                    'Add Product',
-                    style: TextStyle(
-                        color: kPrimaryFont,
-                        fontSize: 45,
-                        fontFamily: 'Alef-Regular'),
-                  ),
-                  Text(
-                    'insert your product list',
-                    style: TextStyle(
-                        color: kPrimaryFont,
-                        fontSize: 20,
-                        fontFamily: 'Alef-Regular'),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  CardProductButton(),
-                ],
+        body: SingleChildScrollView(
+          child: Container(
+            child: Align(
+              alignment: Alignment.center,
+              child: Container(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text(
+                      'Add Product',
+                      style: TextStyle(
+                          color: kPrimaryFont,
+                          fontSize: 45,
+                          fontFamily: 'Alef-Regular'),
+                    ),
+                    Text(
+                      'insert your product list',
+                      style: TextStyle(
+                          color: kPrimaryFont,
+                          fontSize: 20,
+                          fontFamily: 'Alef-Regular'),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    CardProductButton(
+                      press: () {
+                        change();
+                      },
+                    ),
+                    
+                    CardProductButton(
+                      press: () {
+                        change();
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
