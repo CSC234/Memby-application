573cd20cc436da24efa9163d52e240535707ee37
Gorgun <yosathorn.pengma@mail.kmutt.ac.th>
2021-03-22 19:58:45 +0700

    [UPDATE] new view dashboard

--- lib/screens/viewDashboard.dart
+++ lib/screens/viewDashboard.dart
@@ -1,81 +1,37 @@
+import 'package:memby/components/dashboard.dart';
+import 'package:memby/screens/landingScreen.dart';
+// import 'package:memby/components/dashboard.dart';
 import 'package:flutter/material.dart';
-import 'package:memby/constants.dart';
-
-import '../constants.dart';
 import '../constants.dart';
 
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: choices.length,
-        child: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            backgroundColor: kPrimaryMain,
-            title: Text(
-              'View Dashboard',
-              style: TextStyle(
-                  color: kPrimaryColor,
-                  fontSize: 48,
-                  fontFamily: 'Alef-Regular'),
-            ),
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: choices.map<Widget>((Choice choice) {
-                return Tab(
-                  text: choice.title,
-                );
-              }).toList(),
-            ),
-          ),
-          body: TabBarView(
-            children: choices.map((Choice choice) {
-              return Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ChoicePage(
-                    choice: choice,
-                  ));
-            }).toList(),
-          ),
+    return Scaffold(
+      backgroundColor: kPrimaryMain,
+      appBar: AppBar(
+        title: Text(
+          'Back',
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontFamily: 'Alef-Regular'),
+        ),
+        backgroundColor: kPrimaryMain,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return Landing();
+                },
+              ),
+            );
+          },
         ),
       ),
-    );
-  }
-}
-
-class Choice {
-  final String title;
-  const Choice({this.title});
-}
-
-const List<Choice> choices = <Choice>[
-  Choice(title: 'Daily'),
-  Choice(title: 'Monthly'),
-  Choice(title: 'Yearly'),
-];
-
-class ChoicePage extends StatelessWidget {
-  const ChoicePage({Key key, this.choice}) : super(key: key);
-  final Choice choice;
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
-    return Card(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              choice.title,
-              style: textStyle,
-            )
-          ],
-        ),
-      ),
+      body: Body(),
     );
   }
 }
