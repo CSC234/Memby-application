65b9626e084761f7ffeff6241f9703936773a41b
Wirawat Jaiarree <katak6565@gmail.com>
2021-03-23 17:46:07 +0700

    [UPDATE] addProduct

--- lib/components/ProductList.dart
+++ lib/components/ProductList.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dotted_decoration/dotted_decoration.dart';
+import 'package:memby/constants.dart';
 
 class ProductList extends StatelessWidget {
   final double fontsize;
@@ -17,65 +18,93 @@ class ProductList extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     return Column(children: [
       Container(
-        height: 100,
-        width: width * (90 / 100),
         child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10.0),
-                child: Image.asset(
-                  'assets/images/product1.jpg',
-                  width: 70,
+          width: width * (90 / 100),
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image.asset(
+                      'assets/images/product1.jpg',
+                      width: 70,
+                    ),
+                  ),
                 ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding:
-                        EdgeInsets.only(bottom: 5, left: 5, right: 5, top: 10),
-                    child: Text(
-                      "name: " + product,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                    ),
-                  ),
-                  Container(
-                    child: Container(
-                      padding: EdgeInsets.all(5),
-                      alignment: Alignment.topLeft,
-                      width: 200,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(bottom: 5, top: 15),
                       child: Text(
-                        description,
+                        "name: " + product,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
                       ),
                     ),
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(5),
-                        height: 40,
-                        width: 100,
-                        child: Text("pirce: " + price.toString()),
+                    Container(
+                      child: Container(
+                        padding: EdgeInsets.only(top: 10),
+                        alignment: Alignment.topLeft,
+                        width: 200,
+                        child: Text(
+                          description,
+                        ),
                       ),
-                    ],
-                  )
-                ],
-              ),
-            ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(top: 10),
+                          height: 40,
+                          width: 100,
+                          child: Text("pirce: " + price.toString()),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Container(
+                    height: 100,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 43,
+                          child: TextButton(
+                            child: Container(
+                              width: 30,
+                              height: 30,
+                              child: Center(
+                                  child: Text(
+                                '-',
+                                style: TextStyle(
+                                    fontSize: 15, color: Colors.white),
+                              )),
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.grey),
+                            ),
+                            // onPressed: press,
+                          ),
+                        ),
+                      ],
+                    ))
+              ],
+            ),
+            // height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
-          height: 150,
-          decoration: BoxDecoration(
+          decoration: DottedDecoration(
+            shape: Shape.box,
+            color: Color(0xFFB3ABBC),
             borderRadius: BorderRadius.circular(10),
           ),
-        ),
-        decoration: DottedDecoration(
-          shape: Shape.box,
-          color: Color(0xFFB3ABBC),
-          borderRadius: BorderRadius.circular(10),
         ),
       ),
       SizedBox(
