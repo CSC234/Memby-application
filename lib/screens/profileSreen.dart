bb7bb504db3676d234cb588069b20716ecf25853
Wirawat Jaiarree <katak6565@gmail.com>
2021-03-27 17:29:25 +0700

    [UPDATE] profile screen

--- lib/screens/profileSreen.dart
+++ lib/screens/profileSreen.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:memby/components/Card.dart';
-import 'package:memby/screens/viewDashboard.dart';
-import 'package:memby/screens/createOrderScreen.dart';
 import 'package:memby/constants.dart';
-import 'package:memby/screens/addProductScreen.dart';
-import 'package:memby/screens/registerScreen.dart';
-import 'package:memby/screens/homeScreen.dart';
+
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:memby/firebase.dart';
 
 class Profile extends StatelessWidget {
@@ -17,55 +12,126 @@ class Profile extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
 
     return Container(
-      child: Scaffold(
-        body: Stack(
-          children: <Widget>[
-            Positioned(
-              height: height * (40 / 100),
-              left: 170,
-              child: IconButton(
-                onPressed: () {
-                  print("Sign Out Pressed");
-                  context.read<FlutterFireAuthService>().signOut();
-                  if (Navigator.of(context).canPop()) {
-                    Navigator.of(context).pop();
-                  }
-                },
-                icon: Icon(
-                  Icons.exit_to_app,
-                  color: Colors.black,
-                  size: 35,
+        child: Scaffold(
+            body: SizedBox(
+      height: 250,
+      child: Positioned(
+        height: height,
+        child: SizedBox(
+            width: width,
+            child: DecoratedBox(
+                decoration: const BoxDecoration(
+                  color: kPrimaryMain,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40),
+                  ),
                 ),
-              ),
-            ),
-            Positioned(
-              height: height * (40 / 100),
-              left: 220,
-              child: IconButton(
-                onPressed: () {
-                  print("Sign Out Pressed");
-                  context.read<FlutterFireAuthService>().addCustomer(
-                      'Toei',
-                      'Kung',
-                      'toei@mail.com',
-                      '086666666',
-                      '',
-                      'Male',
-                      'kmutt');
-                  if (Navigator.of(context).canPop()) {
-                    Navigator.of(context).pop();
-                  }
-                },
-                icon: Icon(
-                  Icons.people,
-                  color: Colors.black,
-                  size: 35,
-                ),
-              ),
-            ),
-          ],
-        ),
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 35,
+                      ),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          'Your Profile',
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(left: 10),
+                              height: 100,
+                              width: 100,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.grey[300],
+                              )),
+                          Container(
+                            margin: EdgeInsets.only(left: 30),
+                            // padding: EdgeInsets.all(10),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 10),
+                                  width: width * (50 / 100),
+                                  child: Container(
+                                    child: Text("Compnay: Godchapong",
+                                        textAlign: TextAlign.left,
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 18)),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 10),
+                                  width: width * (50 / 100),
+                                  child: Container(
+                                    child: Text("Since: 25-Mar-21",
+                                        textAlign: TextAlign.left,
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 18)),
+                                  ),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(bottom: 10),
+                                  width: width * (50 / 100),
+                                  child: Container(
+                                    child: Text("Last Login: 25-Mar-21",
+                                        textAlign: TextAlign.left,
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 18)),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ))),
       ),
-    );
+      // Container(
+      //                             margin: EdgeInsets.all(10),
+      //                             child: Text(
+      //                               'Company: Godchapong',
+      //                               style: TextStyle(
+      //                                   fontSize: 18, color: Colors.white),
+      //                             ),
+      //                           ),
+      //                           Align(
+      //                             alignment: Alignment.topRight,
+      //                             child: Text('test'),
+      //                           )
+
+      // body: Stack(
+      //   children: <Widget>[
+      //     Positioned(
+      //       height: height * (40 / 100),
+      //       left: 170,
+      //       child: IconButton(
+      //         onPressed: () {
+      //           print("Sign Out Pressed");
+      //           context.read<FlutterFireAuthService>().signOut();
+      //           if (Navigator.of(context).canPop()) {
+      //             Navigator.of(context).pop();
+      //           }
+      //         },
+      //         icon: Icon(
+      //           Icons.exit_to_app,
+      //           color: Colors.black,
+      //           size: 35,
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
+    )));
   }
 }
