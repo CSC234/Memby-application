f6076b51484996cb9b459cf244b44ad1778cae3e
kodchapong1295 <sunsasabest@hotmail.com>
2021-03-29 00:28:19 +0700

    [REFACTOR] extract component from createOrderScreen

--- lib/screens/createOrderScreen.dart
+++ lib/screens/createOrderScreen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:memby/constants.dart';
-import 'package:dotted_decoration/dotted_decoration.dart';
 import 'package:memby/models/Product.dart';
 import 'package:memby/models/OrderDetail.dart';
 import 'package:memby/models/Order.dart';
+import 'package:memby/components/RoundedButton.dart';
+import 'package:memby/components/OrderCard.dart';
+import 'package:memby/components/ProductBox.dart';
 
 //Mock up data
 List<Product> Products = [
@@ -66,7 +68,7 @@ class _CreateOrderScreenState extends State<CreateOrderScreen> {
           title: p.productName,
           isFilled: p.isFilled,
           onPress: () {
-            handleClicked(i);
+            handleClickedProductItem(i);
           },
         ),
       );
@@ -77,7 +79,7 @@ class _CreateOrderScreenState extends State<CreateOrderScreen> {
     );
   }
 
-  void handleClicked(int index) {
+  void handleClickedProductItem(int index) {
     setState(() {
       Products[index].isFilled = !Products[index].isFilled;
       if (Products[index].isFilled == true) {
@@ -92,11 +94,6 @@ class _CreateOrderScreenState extends State<CreateOrderScreen> {
         }
         order1.orderDetails.removeAt(deletedIndex);
       }
-
-      // for (Product p in selectedProduct) {
-      //   print(p.productName);
-      // }
-      // print('======');
     });
   }
 
@@ -218,182 +215,8 @@ class _CreateOrderScreenState extends State<CreateOrderScreen> {
   }
 }
 
-class RoundedButton extends StatelessWidget {
-  final Function onPress;
-  final String title;
-  final Color color;
 
-  RoundedButton({this.color, this.onPress, this.title});
 
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPress,
-      child: Text(title),
-      style: ButtonStyle(
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-        ),
-        backgroundColor: MaterialStateProperty.resolveWith<Color>(
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.pressed)) return color;
-            return color; // Use the component's default.
-          },
-        ),
-      ),
-    );
-  }
-}
 
-class OrderCard extends StatelessWidget {
-  OrderCard(
-      {this.title,
-      this.img,
-      this.description,
-      this.price,
-      this.amount,
-      this.setAmount});
 
-  final String title;
-  final String img;
-  final double price;
-  final String description;
-  final int amount;
-  final Function setAmount;
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.only(right: 20, left: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Image.asset(
-                        'assets/images/${img}.jpg',
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 6,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        title,
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        child: Container(
-                          alignment: Alignment.topLeft,
-                          width: 200,
-                          child: Text(
-                            description,
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('$price Baht'),
-                          Container(
-                            child: TextField(
-                              keyboardType: TextInputType.number,
-                              textAlign: TextAlign.center,
-                              onChanged: (value) {
-                                setAmount(int.parse(value));
-                              },
-                              controller: TextEditingController()
-                                ..text = amount.toString(),
-                              decoration: kTextFieldDecoration.copyWith(
-                                hintText: 'amount',
-                              ),
-                            ),
-                            width: 100,
-                            height: 40,
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          height: 150,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        decoration: DottedDecoration(
-          shape: Shape.box,
-          color: Color(0xFFB3ABBC),
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ),
-    );
-  }
-}
-
-class ProductBox extends StatelessWidget {
-  ProductBox({this.img, this.title, this.isFilled, this.onPress});
-  final String img;
-  final String title;
-  final bool isFilled;
-  final Function onPress;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5),
-      child: GestureDetector(
-        child: Container(
-          child: Container(
-            padding: EdgeInsets.all(8),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(10.0),
-                  child: Image.asset(
-                    'assets/images/$img.jpg',
-                    width: 100,
-                  ),
-                ),
-                Text(title),
-              ],
-            ),
-            height: 150,
-            decoration: BoxDecoration(
-              color: isFilled ? Color(0xFFDDDDDD) : null,
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          decoration: DottedDecoration(
-            shape: Shape.box,
-            color: Color(0xFFB3ABBC),
-            borderRadius:
-                BorderRadius.circular(10), //remove this to get plane rectange
-          ),
-        ),
-        onTap: onPress,
-      ),
-    );
-  }
-}
