4dc6b8d60fea6b86678a94a1da728e3f41b882f7
best <sunsasabest@hotmail.com>
2021-04-11 21:45:29 +0700

    [DRAFT] reciept

--- lib/screens/orderRecieptScreen.dart
+++ lib/screens/orderRecieptScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:memby/components/Profile/main.dart';
 import 'package:memby/models/Order.dart';
 import 'package:memby/constants.dart';
 import 'package:memby/components/OrderCard.dart';
@@ -9,29 +10,68 @@ class OrderRecieptScreen extends StatelessWidget {
   final Order order;
   OrderRecieptScreen({this.order});
 
-  ListView makeOrderCard() {
-    List<OrderCard> orderCards = [];
+  double getTotalPrice() {
+    double totalPrice = 0;
+    for (OrderDetail i in order.orderDetails) {
+      totalPrice += i.amount * i.product.price;
+    }
+    return totalPrice;
+  }
 
+  ListView makeRecieptDescription() {
+    List<ListTile> itemsListTile = [];
+    int count = 1;
+    itemsListTile.add(
+      ListTile(
+        leading: Text(''),
+        title: Text(
+          'Details (price)',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        trailing: Text(
+          'Amount',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
     ////////////////////
     for (OrderDetail o in order.orderDetails) {
       Product p = o.product;
       int a = o.amount;
       // print(p.productName + "\n amount: " + a.toString());
-      orderCards.add(
-        OrderCard(
-          title: p.productName,
-          img: p.img,
-          description: p.description,
-          price: p.price,
-          amount: a,
-          setAmount: o.setAmount,
+      itemsListTile.add(
+        ListTile(
+          leading: Text('$count. '),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(p.productName + ' (${p.price} THB)'),
+              Text(a.toString())
+            ],
+          ),
         ),
       );
+      count++;
     }
-    print('====');
+    itemsListTile.add(
+      ListTile(
+        trailing: Column(
+          children: [
+            Text(
+              'Total',
+            ),
+            Text(
+              '${getTotalPrice().toString()} THB',
+              style: TextStyle(color: Colors.red),
+            ),
+          ],
+        ),
+      ),
+    );
+    // print('====');
     return ListView(
       scrollDirection: Axis.vertical,
-      children: orderCards,
+      children: itemsListTile,
     );
   }
 
@@ -45,7 +85,7 @@ class OrderRecieptScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Confirm Order',
+                  'Reciept',
                   style: kPrimaryHeadingTextStyle,
                 ),
               ],
@@ -53,26 +93,50 @@ class OrderRecieptScreen extends StatelessWidget {
             SizedBox(
               height: 15,
             ),
-            // Padding(
-            //   padding: const EdgeInsets.only(left: 20),
-            //   child: Row(
-            //     mainAxisAlignment: MainAxisAlignment.start,
-            //     children: [
-            //       Text(
-            //         'Confirmed Products',
-            //         style: kPrimaryHeadingTextStyle.copyWith(
-            //             fontSize: 20, fontWeight: FontWeight.normal),
-            //       ),
-            //     ],
-            //   ),
-            // ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Member ID: 193330000',
+                        style: kPrimaryHeadingTextStyle.copyWith(
+                            fontSize: 20, fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Member Name: Kodchapong',
+                        style: kPrimaryHeadingTextStyle.copyWith(
+                            fontSize: 20, fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Date: 14/04/2021',
+                        style: kPrimaryHeadingTextStyle.copyWith(
+                            fontSize: 20, fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
             Expanded(
               flex: 8,
               child: Padding(
                 padding: const EdgeInsets.all(10.0),
                 child: order.orderDetails.isEmpty
                     ? Text('Empty Order')
-                    : makeOrderCard(),
+                    : makeRecieptDescription(),
               ),
             ),
           ],
